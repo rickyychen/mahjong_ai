c245c145f2ec67df252ae9b5b8883988f5065db1
unknown <chen.rickyy@gmail.com>
2024-05-11 23:01:10 -0400

    complete deck initialization

--- backend/src/mahjong_environment/Game.py
+++ backend/src/mahjong_environment/Game.py
@@ -5,6 +5,6 @@ from random import shuffle
 class Game:
 
     def __init__(self):
-        self.tiles = [Tile(i, j) for i in Tile.TILE.keys() for j in Tile.TILE[i]]
+        self.tiles = [Tile(i, j) for i in Tile.TILE.keys() for j in Tile.TILE[i] for k in range(Tile.TILE_COUNT[i])]
         shuffle(self.tiles)
         self.deck = Hand(self.tiles)
