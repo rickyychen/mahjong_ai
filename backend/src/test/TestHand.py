ec95f9f4ebe2795c36bb954c7ecef255ded2f917
unknown <chen.rickyy@gmail.com>
2024-05-15 00:02:11 -0400

    add more testcases for is_win() and fix is_win issue and add gitignore files

--- backend/src/test/TestHand.py
+++ backend/src/test/TestHand.py
@@ -8,12 +8,28 @@ from Hand import Hand
 class TestHand(unittest.TestCase):
     
     def setUp(self):
-        tile1_0 = Tile('C', 1)
-        tile1_1 = Tile('C', 1)
-        self.hand = Hand([tile1_0, tile1_1])
+        self.tile_c1 = Tile('C', 1)
+        self.tile_c2 = Tile('C', 2)
+        self.tile_c3 = Tile('C', 3)
+        self.tile_c4 = Tile('C', 4)
     
-    def testIsWin(self):
-        self.assertTrue(self.hand.is_win())
+    def testIsWinPairNonFlush(self):
+        hand = Hand([self.tile_c1, self.tile_c1])
+        self.assertTrue(hand.is_win())
+
+    def testIsWinNonPairFlush(self):
+        hand = Hand([self.tile_c1, self.tile_c2, self.tile_c3])
+        self.assertFalse(hand.is_win())
+
+    def testIsWinPairFlush(self):
+        hand = Hand([self.tile_c1, self.tile_c1, self.tile_c2, self.tile_c3, self.tile_c4])
+        self.assertTrue(hand.is_win())
+        hand = Hand([self.tile_c1, self.tile_c1, self.tile_c1, self.tile_c2, self.tile_c3])
+        self.assertTrue(hand.is_win())
+    
+    def testIsWinTwoPairs(self):
+        hand = Hand([self.tile_c1, self.tile_c1, self.tile_c2, self.tile_c2])
+        self.assertFalse(hand.is_win())
 
 if __name__ == "__main__":
     unittest.main()
